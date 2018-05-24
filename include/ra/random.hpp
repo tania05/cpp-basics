3c28c355ec9d9300b566fa2e5f99a8f47a8f09a9
Tania Akter <tania9309@gmail.com>
2018-05-24 15:18:06 -0700

    Added header files and the rest of the files, added declaration of the functions as well

---
+++ include/ra/random.hpp
@@ -0,0 +1,44 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+#include <iostream>
+
+class linear_congruential_generator { 
+		linear_congruential_generator(int_type multiplier, int_type increment, int_type modulus, int_type seed = default_seed());
+		linear_congruential_generator(linear_congruential_generator&&) = default;
+		linear_contruential_generator(linear_congruential_generator&) = default;
+		~linear_congruential_generator() = default;
+		
+		//Returns the multiplier value a 
+		int_type multiplier();
+		
+		//returns the increment value c
+		int_type increment();
+		
+		//returne the modulus value m
+		int_type modulus();
+		
+		//Returns the default seed value x0
+		static int_type default_seed();
+		
+		//Advances the generator to the next position
+		int_type operator()();
+		
+		//Discards the next n numbers in the generated sequence
+		void discard(unsigned long long n);
+		
+		//Returns the potential smallest value
+		int_type min();
+		
+		//Returns the potential largest value
+		int_type max();
+		
+		//Equality
+		bool operator==(const linear_congruential_generator& other);
+		
+		//Inequality
+		bool operator!=(const linear_congruential_generator& other);
+		
+		ostream& operator<<(ostream& os, const linear_congruential_generator& generator)
+			
+}
+
