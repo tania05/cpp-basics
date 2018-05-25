07db43e02e9dca52f20d6087b152a7c46b851200
Tania Akter <tania9309@gmail.com>
2018-05-25 11:47:56 -0700

    tyring to see why sanitizer fails on validation

---
+++ Sanitizers.cmake
@@ -0,0 +1,11 @@
+option(ENABLE_ASAN "Initial Address Sanitizer" false)
+if (ENABLE_ASAN)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address")
+endif()
+
+option(ENABLE_UBSAN "Initial Undefined Behaviour Sanitizer" false)
+if (ENABLE_UBSAN)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=undefined")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=undefined")
+endif()
