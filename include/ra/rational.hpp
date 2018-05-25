4dd9a41949c8b92d9c5e995af6a5c63f50172117
Tania Akter <tania9309@gmail.com>
2018-05-25 02:19:57 -0700

    added constructors and operators

--- include/ra/rational.hpp
+++ include/ra/rational.hpp
@@ -1,3 +1,27 @@
-#ifndef RATIONAL_H
-#define RANIONAL_H
+#include <iostream>
 
+namespace ra{
+namspace math{
+    template <typename T>
+    class rational
+    {
+        public:
+            using int_type = T;
+            rational() : n_(0), d_(0) {}
+            rational(int_type n, int_type d = 1) :  n_(n), d_(d)
+            {
+                //TODO
+            }
+            rational(rational&&) = default;
+            rational& operator=(rational&&) = default;
+
+            rational(rational&) = default;
+            rational& operator=(rational&) = default;
+            
+            ~rational() = default;
+        private:
+            int_type n_;
+            int_type d_;
+    }
+}
+}
